84a9e89debe0ec8b3bf6fdf23c2017e821cba0c1
Leon Hillmann <l.hillmann@tue.nl>
2025-12-04 14:23:17 +0100

    test: slightly more complicated example for `detect_t1_events`

--- test/test_t1_detection.py
+++ test/test_t1_detection.py
@@ -29,3 +29,17 @@ def test_simple_t1():
     t1_events = detect_t1_events(adj1, adj2)
     assert len(t1_events) == 1
     assert t1_events[0] == (1, 2, 3, 4)
+
+
+def test_simple_t1_with_extra():
+    adj1 = {1: {2, 4}, 2: {1, 3, 4}, 3: {2, 4}, 4: {1, 2, 3}, 5: {1, 3}}
+    adj2 = {
+        1: {2, 3, 4},
+        2: {1, 3},
+        3: {1, 2, 4},
+        4: {1, 3},
+        5: {1, 2},
+    }
+    t1_events = detect_t1_events(adj1, adj2)
+    assert len(t1_events) == 1
+    assert t1_events[0] == (1, 2, 3, 4)
